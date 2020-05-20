675c6ea7d059ff06772dbe03e631a8d54b3eb8a1
Wexort <bugwriter@outlook.com>
2020-05-20 12:29:25 +0200

    update

--- upload_helm_builder_image.sh
+++ upload_helm_builder_image.sh
@@ -5,5 +5,7 @@ echo "Entey the gcp project name you want to push the helm builder image"
 read -p "gcp project name" project_name
 let gcr_address=gcr.io/${project_name}/helm
 docker build -t ${gcr_address} .
-echo "Start to upload helm builder image for gcp cloudbuild..."
+echo "Start to upload helm builder image for gcp cloudbuild"
+echo "to ${gcr_address}..."
 docker push ${gcr_address}
+echo "Job done!"
